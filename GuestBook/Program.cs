277f88981b8468a7170beda208967b871516b1ed
Nastya808 <nastyaergenova@icloud.com>
2024-08-22 17:52:56 +0300

    program

--- GuestBook/Program.cs
+++ GuestBook/Program.cs
@@ -8,28 +8,23 @@ using GuestBookApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Добавление DbContext с использованием SQLite
 builder.Services.AddDbContext<GuestBookContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Регистрация репозиториев
 builder.Services.AddScoped<IRepository<User>, Repository<User>>();
 builder.Services.AddScoped<IRepository<Message>, Repository<Message>>();
 
-// Добавление Razor Pages
 builder.Services.AddRazorPages();
 
-// Добавление сессий
 builder.Services.AddSession(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // Установите время ожидания
+    options.IdleTimeout = TimeSpan.FromMinutes(30); 
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
 
 var app = builder.Build();
 
-// Конфигурация HTTP конвейера
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -43,7 +38,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-// Использование сессий
 app.UseSession();
 
 app.MapRazorPages();
